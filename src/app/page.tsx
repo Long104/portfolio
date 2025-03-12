0dc103e4bc484a4aa6b62f5e970ede8017a12e72
pantorn <longpantorn@gmail.com>
2025-03-12 22:58:43 +0700

    fix reposive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ import { Clock } from "@/components/ui/clock";
 export default function Home() {
 	return (
 		<div className="relative w-full">
-			<div className="fixed top-12 left-0 right-0 w-[90vw] z-10 flex justify-end justify-self-center p-3 px-6">
-				<Clock />
+			<div className="w-full flex justify-center">
+				<div className="fixed top-12 w-[90vw] z-50 flex justify-end p-3 px-6">
+					<Clock />
+				</div>
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col justify-center items-center">
 				<HomePage />
 				<About />
 				<Project />
--- src/components/page/home.tsx
+++ src/components/page/home.tsx
@@ -5,12 +5,17 @@ import { motion } from "motion/react";
 const home = () => {
 	return (
 		<motion.div
-			className="relative h-svh w-svw"
+			className="relative h-svh w-svw flex justify-center"
 			id="home"
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ duration: 3 }}
 		>
+			<div className="w-[90vw] flex h-full items-start">
+				<div className="bg-secondary sticky top-12 p-3 px-6 rounded-md text-2xl font-bold mt-8 drop-shadow-xl z-10 ">
+					Home
+				</div>
+			</div>
 			<Image
 				src={"/test1.jpg"}
 				width={4999}
@@ -18,11 +23,6 @@ const home = () => {
 				alt="test"
 				className="absolute inset-0 h-full object-cover w-screen brightness-[0.75] opacity-50 "
 			/>
-			<div className="w-[90vw] flex justify-self-center h-full items-start">
-				<div className="bg-secondary sticky top-12 p-3 px-6 rounded-md text-2xl font-bold mt-8 drop-shadow-xl z-10 ">
-					Home
-				</div>
-			</div>
 			<div className="pb-4 text-3xl z-50 flex absolute inset-0 justify-center h-full items-center font-bold">
 				Welcome, to my world!
 			</div>
