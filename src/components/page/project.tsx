40a042a737ffef8184aea0d0d95ec50526f4bd9d
pantorn <longpantorn@gmail.com>
2025-05-05 13:24:10 +0700

    senzen to cashwise

--- src/components/page/project.tsx
+++ src/components/page/project.tsx
@@ -10,42 +10,10 @@ const project = () => {
 			<div className="bg-secondary sticky top-12 p-3 px-6 rounded-md text-2xl font-bold mt-8 drop-shadow-xl">
 				Project
 			</div>
-			<div className="grid lg:grid-cols-2 grid-cols-1 place-items-center w-full mt-8">
-				<div>
-					<div className="text-4xl font-bold flex items-center">
-						notion-clone:
-						<Link
-							href={"https://github.com/Long104/notion-clone"}
-							className="text-2xl bg-foreground text-background rounded-md px-1"
-							target="_blank"
-						>
-							github
-						</Link>
-						<Link
-							href={"https://notion-clone-rose-five.vercel.app/"}
-							target="_blank"
-							className="text-2xl bg-foreground text-background rounded-md px-1 ml-1"
-						>
-							app
-						</Link>
-					</div>
-					A clone of notion with functionality like sharing document with friend
-					and using Ai for translation. The main technology here is clerk,
-					liveblock, blocknote, firebase, cloudflare for ai worker and deploying{" "}
-				</div>{" "}
-				<Image
-					src="https://i.imgur.com/Nq9bWQ2.png"
-					alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
-					className="rounded-2xl mt-8 lg:mt-0"
-					width={500}
-					height={350}
-				/>
-			</div>
-
 			<div className="grid lg:grid-cols-2 grid-cols-1 place-items-center w-full mt-8 pb-4">
 				<div>
 					<div className="text-4xl font-bold flex items-center">
-						senzen:
+						cashwise:
 						<Link
 							href={"https://github.com/Long104/SenZen"}
 							className="text-2xl bg-foreground text-background rounded-md px-1"
@@ -74,7 +42,6 @@ const project = () => {
 					height={350}
 				/>
 			</div>
-
 			<div className="grid lg:grid-cols-2 grid-cols-1 place-items-center w-full mt-8 pb-4">
 				<div>
 					<div className="text-4xl font-bold flex items-center">
@@ -94,6 +61,37 @@ const project = () => {
 					src="https://i.imgur.com/n0Xsxxz.png"
 					alt="ai extension"
 					className="rounded-2xl mt-8 lg:m-0"
+					width={500}
+					height={350}
+				/>
+			</div>
+			<div className="grid lg:grid-cols-2 grid-cols-1 place-items-center w-full mt-8">
+				<div>
+					<div className="text-4xl font-bold flex items-center">
+						notion-clone:
+						<Link
+							href={"https://github.com/Long104/notion-clone"}
+							className="text-2xl bg-foreground text-background rounded-md px-1"
+							target="_blank"
+						>
+							github
+						</Link>
+						<Link
+							href={"https://notion-clone-rose-five.vercel.app/"}
+							target="_blank"
+							className="text-2xl bg-foreground text-background rounded-md px-1 ml-1"
+						>
+							app
+						</Link>
+					</div>
+					A clone of notion with functionality like sharing document with friend
+					and using Ai for translation. The main technology here is clerk,
+					liveblock, blocknote, firebase, cloudflare for ai worker and deploying{" "}
+				</div>{" "}
+				<Image
+					src="https://i.imgur.com/Nq9bWQ2.png"
+					alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
+					className="rounded-2xl mt-8 lg:mt-0"
 					width={500}
 					height={350}
 				/>
